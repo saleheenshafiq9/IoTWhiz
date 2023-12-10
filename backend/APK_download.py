8a148e48c60c8c4ae269ab740beb748a5ad3c8a4
Shafiq Saleheen <bsse1125@iit.du.ac.bd>
2023-12-10 18:16:10 +0600

    ready for automation

--- backend/APK_download.py
+++ backend/APK_download.py
@@ -1,17 +1,15 @@
 import requests
-
-# api_key = '36b0de1dba01073d4a53a50d2060e79a0efc33337e95b3d7b7a448f7f6665e4e'
-# sha256 = '23622BAADC12C422D70DA7AA7B6F0FF7B16F75C19E5B81F8B2F57A9F0E77151C'
+from tqdm import tqdm  # Import tqdm for progress bar
 
 def download_apk(api_key, sha256):
     url = f"https://androzoo.uni.lu/api/download?apikey={api_key}&sha256={sha256}"
 
-    response = requests.get(url)
+    response = requests.get(url, stream=True)  # Use stream=True to enable streaming
 
     if response.status_code == 200:
         # Extract filename or identifier to use for saving the file
         content_disposition = response.headers.get('content-disposition')
-        file_size = response.headers.get('content-length')
+        file_size = int(response.headers.get('content-length'))  # Get the file size
         if content_disposition:
             filename = content_disposition.split("filename=")[1]
             filename = filename.strip('"')  # Clean up the filename if needed
@@ -19,8 +17,19 @@ def download_apk(api_key, sha256):
             # If content disposition header isn't available, use the sha256 value
             filename = f"{sha256}.apk"
 
-        with open(filename, "wb") as file:
-            file.write(response.content)
+        # Use tqdm to create a progress bar for the download
+        with open(filename, "wb") as file, tqdm(
+            total=file_size,
+            unit='B',
+            unit_scale=True,
+            desc=filename,
+            ascii=True,
+            ncols=100
+        ) as progress_bar:
+            for data in response.iter_content(chunk_size=1024):
+                file.write(data)
+                progress_bar.update(len(data))  # Update the progress bar
+
         print(f"File '{filename}' downloaded successfully!")
         return filename, file_size
     else:
